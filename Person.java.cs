a1e2ea81527aa8035f1d29c5d68beb55b4fd35e8
HamaadZahid <147430798+HamaadZahid@users.noreply.github.com>
2024-09-06 14:07:33 -0500

    Add files via upload

---
+++ Person.java.cs
@@ -0,0 +1,34 @@
+﻿public class Person
+{
+    private String firstName;
+    private String lastName;
+    private String id;
+
+    public Person(String firstName, String lastName, String id)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.id = id;
+    }
+
+    public String getFirstName()
+    {
+        return firstName;
+    }
+
+    public String getLastName()
+    {
+        return lastName;
+    }
+
+    public String getId()
+    {
+        return id;
+    }
+
+    
+    public String toString()
+    {
+        return "Person [firstName=" + firstName + ", lastName=" + lastName + ", id=" + id + "]";
+    }
+}
